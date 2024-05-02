a6cfb67d4b4009af95ec5a1dd457c417210aa755
DimitriCamargo-Developer <101725459+DimitriCamargo-Developer@users.noreply.github.com>
2024-05-01 20:29:59 -0400

    Utilização da Interpolação
    S3A27

--- projeto-inicial/src/app/card/card.component.ts
+++ projeto-inicial/src/app/card/card.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  plano = { //OBJETO QUE POSSUI PROPS QUE POSSUI OUTROS OBJS
+    infos: {
+      tipo: 'Simples',
+      preco: 100
+    }   
+  };
 
+  // plano = { # PARTE 2
+  //   tipo: 'Simples',
+  //   preco: 100
+  // };
+
+    //   tipo = 'Teste'; # PARTE 1
+    // preco = 500;
+    // setTimeout(() => { 
+    //   console.log('Set Timeout')
+    //   this.tipo = 'Simples'
+    // }, 4000);
+  //   return 'R$' + this.preco + ',00/Mês';
+  // }
 }
