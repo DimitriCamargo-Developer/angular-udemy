aca987285d60fd2627869302addeae99e11625ea
DimitriCamargo-Developer <101725459+DimitriCamargo-Developer@users.noreply.github.com>
2024-05-14 18:36:55 -0400

    S3A28: Optional Channing - Encadeamento Opcional na interpolação

--- projeto-inicial/src/app/card/card.component.ts
+++ projeto-inicial/src/app/card/card.component.ts
@@ -1,29 +1,25 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos{
+  tipo: string;
+  preco: number;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = { //OBJETO QUE POSSUI PROPS QUE POSSUI OUTROS OBJS
+  // @ts-ignore
+  plano: IPlano = {
     infos: {
       tipo: 'Simples',
-      preco: 100
-    }   
+      preco: 100,
+    },
   };
-
-  // plano = { # PARTE 2
-  //   tipo: 'Simples',
-  //   preco: 100
-  // };
-
-    //   tipo = 'Teste'; # PARTE 1
-    // preco = 500;
-    // setTimeout(() => { 
-    //   console.log('Set Timeout')
-    //   this.tipo = 'Simples'
-    // }, 4000);
-  //   return 'R$' + this.preco + ',00/Mês';
-  // }
 }
